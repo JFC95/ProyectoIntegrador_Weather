6b90087b9daa87d20c73e51dfdab03b150194e52
joafunes <funes.joaquin94@gmail.com>
2021-05-05 20:24:02 -0300

    add tag_lluvia

--- wrf-cuenca/src/exec_cuencas.sh
+++ wrf-cuenca/src/exec_cuencas.sh
@@ -2,8 +2,8 @@
 #WRFOUT="/opt/wrfdatos/wrfout/2020_05/wrfout_A_d01_2020-05-27_06:00:00"
 WRFOUT="/opt/wrfdatos/wrfout/2021_05/wrfout_A_d01_2021-05-05_06:00:00.nc"
 
-OUTDIR_PRODUCTOS="/home/Cavanagh_jf/csv_cuencas/productos"
-OUTDIR_TABLA="/home/Cavanagh_jf/csv_cuencas/tablas"
+OUTDIR_PRODUCTOS="temp/productos"
+OUTDIR_TABLA="temp/tablas/"
 # La configuracion ahora es un parametro opcional, si no se pasa, se obtiene automaticamente del wrfout con el formato
 # CBA_{PARAMETRIZACION}_{HORA}
 #CONFIGURACION="CBA_A_06"
