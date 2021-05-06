017aa17ae879c03bde944106938d382962ba418f
JFC95 <juan.cavanagh@mi.unc.edu.ar>
2021-05-06 11:55:42 -0300

    csv por año-mes-dia

--- wrf-cuenca/src/exec_cuencas.sh
+++ wrf-cuenca/src/exec_cuencas.sh
@@ -7,8 +7,8 @@ OUTDIR_TABLA="temp/tablas/"
 CONFIGURACION6="CBA_A_06"
 CONFIGURACION18="CBA_A_18"
 
-DIA06="/opt/wrfdatos/wrfout/2021_*/wrfout_A_*_06*"
-DIA18="/opt/wrfdatos/wrfout/2021_*/wrfout_A_*_18*"
+DIA06="/opt/wrfdatos/wrfout/20*/wrfout_A_*_06*"
+DIA18="/opt/wrfdatos/wrfout/20*/wrfout_A_*_18*"
 ray start --head --port=6379 --num-cpus=6
 for i in $DIA06; do
     time python cuencas_wrf.py $i ${OUTDIR_PRODUCTOS}  ${OUTDIR_TABLA} --configuracion ${CONFIGURACION6}
