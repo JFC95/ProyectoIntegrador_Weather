1e0b8d41422266b5ac700d84f0d6d395c9a082d7
juancavanagh5 <juancavanagh40@gmail.com>
2021-05-06 11:33:46 -0300

    ppn a csv, 06 and 18 hs x mes

--- wrf-cuenca/src/exec_cuencas.sh
+++ wrf-cuenca/src/exec_cuencas.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-#WRFOUT="/opt/wrfdatos/wrfout/2020_05/wrfout_A_d01_2020-05-27_06:00:00"
-WRFOUT="/opt/wrfdatos/wrfout/2021_05/wrfout_A_d01_2021-05-05_06:00:00.nc"
 
 OUTDIR_PRODUCTOS="temp/productos"
 OUTDIR_TABLA="temp/tablas/"
 # La configuracion ahora es un parametro opcional, si no se pasa, se obtiene automaticamente del wrfout con el formato
 # CBA_{PARAMETRIZACION}_{HORA}
-#CONFIGURACION="CBA_A_06"
+CONFIGURACION6="CBA_A_06"
+CONFIGURACION18="CBA_A_18"
 
+DIA06="/opt/wrfdatos/wrfout/2021_*/wrfout_A_*_06*"
+DIA18="/opt/wrfdatos/wrfout/2021_*/wrfout_A_*_18*"
 ray start --head --port=6379 --num-cpus=6
+for i in $DIA06; do
+    time python cuencas_wrf.py $i ${OUTDIR_PRODUCTOS}  ${OUTDIR_TABLA} --configuracion ${CONFIGURACION6}
+done
+for j in $DIA18; do
+    time python cuencas_wrf.py $j ${OUTDIR_PRODUCTOS}  ${OUTDIR_TABLA} --configuracion ${CONFIGURACION18}
 
-time python cuencas_wrf.py ${WRFOUT} ${OUTDIR_PRODUCTOS}  ${OUTDIR_TABLA} # --configuracion ${CONFIGURACION}
-
-ray stop
+done
+ray stop 
